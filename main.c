9fc82e11593893863f9bd2cde49d960013e31d07
cuki2 <mauricio@krebs.com.br>
2015-05-12 13:16:41 -0300

    gitignore added

--- main.c
+++ main.c
@@ -11,14 +11,17 @@
 
 #use delay(clock=15MHz)
 #use rs232(baud=9600, uart1)
-#use spi(MASTER, SPI1,  BAUD=9600, MODE=1, FORCE_HW)
 
 int main(void) {
+
+//	setup_spi(SPI_MASTER | SPI_H_TO_L | SPI_CLK_DIV_4);
+	setup_spi(SPI_MASTER | SPI_H_TO_L | SPI_CLK_DIV_16);
 
 	while (true) {
 
 		putc(0x55);
 		spi_write(0x55);
+		output_toggle(PIN_B0);
 		delay_ms(1000);
 	}
 
